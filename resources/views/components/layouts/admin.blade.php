bf8deaae0cd337f366be8f4384033a6e72fe1d9c
ryfnpshy <m.rayfanpashya00@gmail.com>
2025-06-03 00:06:23 +0700

    admin

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -210,9 +210,9 @@
                 <div class="relative">
                     <button type="button" class="-m-1.5 flex items-center p-1.5" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                     <span class="sr-only">Open user menu</span>
-                    <img class="size-8 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                    <img class="size-8 rounded-full bg-gray-50" src="https://i.pinimg.com/736x/2b/59/40/2b594036c4b9f60ac0d33a0deffa3eb2.jpg" alt="">
                     <span class="hidden lg:flex lg:items-center">
-                        <span class="ml-4 text-sm/6 font-semibold text-gray-900" aria-hidden="true">Tom Cook</span>
+                        <span class="ml-4 text-sm/6 font-semibold text-gray-900" aria-hidden="true">Admin</span>
                         <svg class="ml-2 size-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                         <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
                         </svg>
