3495ea08530ca1c45b23b920eb58973d55190836
Yudi Hokiana <yuddihokkiana@gmail.com>
2025-06-04 10:02:16 +0700

    Update index.blade.php

--- resources/views/auth/admin/users/index.blade.php
+++ resources/views/auth/admin/users/index.blade.php
@@ -18,7 +18,7 @@
 
     <div class="px-4 sm:px-6 lg:px-8">
         <div class="sm:flex sm:items-center">
-            <div class="sm:flex-auto"></div>
+            <div class="sm:flex-auto"></div> 
         <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
         <a href="/admin/user/create" type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add user</a>
     </div>
