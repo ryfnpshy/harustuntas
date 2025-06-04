37e60a1d636cb6970cc210331abc86d5fe935444
Yudi Hokiana <yuddihokkiana@gmail.com>
2025-06-04 09:52:15 +0700

    iseng aja

--- resources/views/auth/admin/users/create.blade.php
+++ resources/views/auth/admin/users/create.blade.php
@@ -28,7 +28,7 @@
                     <label for="username" class="block text-sm/6 font-medium text-gray-900 sm:pt-1.5">Username</label>
                     <div class="mt-2 sm:col-span-2 sm:mt-0">
                         <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 sm:max-w-md">
-                        <input type="text" name="username" id="username" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="janesmith">
+                        <input type="text" name="username" id="username" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="tud sulaeman">
                         </div>
                     </div>
                 </div>
